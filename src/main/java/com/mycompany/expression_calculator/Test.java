a18d3b4c94c1e418cebbeab6e98f0807036a2db7
SpaceBa13 <brayanalpizar36@gmail.com>
2023-10-27 22:57:45 -0600

    bug fix

--- src/main/java/com/mycompany/expression_calculator/Test.java
+++ src/main/java/com/mycompany/expression_calculator/Test.java
@@ -19,9 +19,9 @@ public class Test {
     public String image_processed() {
         Tesseract tesseract = new Tesseract();
         try {
-            tesseract.setDatapath("C:\\Users\\darig\\OneDrive\\Documentos\\TEC\\Semestre 2\\Algoritmos y Estructuras de Datos I\\Proyecto II\\Expression_Calculator\\Tess4J-3.4.8-src\\Tess4J\\tessdata");
+            tesseract.setDatapath("Tess4J-3.4.8-src\\Tess4J\\tessdata");
 
-            text = tesseract.doOCR(new File("C:\\Users\\darig\\OneDrive\\Documentos\\TEC\\Semestre 2\\Algoritmos y Estructuras de Datos I\\Proyecto II\\Expression_Calculator\\Images\\photo.jpg"));
+            text = tesseract.doOCR(new File("Images\\photo.jpg"));
             text = text.replaceAll("—", "-");
 
             System.out.println(text);
